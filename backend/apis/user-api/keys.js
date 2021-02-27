b2dfa54512e0865fc6ebfee0a6638a94016862e8
Fabian Desnoes <fabiandes@hotmail.co.uk>
2021-02-27 11:45:53 +0000

    env variables

--- backend/apis/user-api/keys.js
+++ backend/apis/user-api/keys.js
@@ -1,3 +1,8 @@
 module.exports = {
-    port: process.env.PORT || 4040
+    port: process.env.PORT || 4040,
+    dbUser: process.env.DB_USER,
+    dbHost: process.env.DB_HOST, 
+    dbName: process.env.DB_NAME, 
+    dbPassword: process.env.DB_PASSWORD, 
+    dbPort: process.env.DB_PORT
 }
